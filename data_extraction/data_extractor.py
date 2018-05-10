8b95fcc78cd68aa54af18a2958f8a27dfeb55fb1
samuel-campbell <samuel.pcampbell@gmail.com>
2018-05-10 17:02:47 -0400

    created unique columns for inputs and associated numerical values to outputs

--- data_extraction/data_extractor.py
+++ data_extraction/data_extractor.py
@@ -1,10 +1,12 @@
 import csv
+import pandas
+import json
 
 
 class Parser:
-    training_data_file = "training_data_example.csv"
-    validation_data_file = "validation_data_example.csv"
-    employee_file = "employee.csv"
+    training_data_file = "../data/training_data_example.csv"
+    validation_data_file = "../data/validation_data_example.csv"
+    employee_file = "../data/employee.csv"
 
     def __init__(self):
         pass
@@ -22,4 +24,47 @@ class Parser:
                 else:
                     data_list.append(row)
                 i = i + 1
-        return data_list, header_list
+        return Parser.__pre_process_data(data_list, header_list)
+
+    @staticmethod
+    def __pre_process_data(data_list, header_list):
+        """
+        ['date', 'category', 'employee id', 'expense description', 'pre-tax amount', 'tax name', 'tax amount']
+        :param data_list:
+        :return:
+        """
+        table = pandas.DataFrame(data_list, columns=header_list)
+        table.drop(['date', 'employee id'], axis=1, inplace=True)
+        unique_categories = table['category'].unique()
+        unique_expense_desc = table['expense description'].unique()
+        unique_tax_name = table['tax name'].unique()
+
+        column_index = {
+            'input': {},
+            'output': {}
+        }
+
+        column_index['input']['pre-tax amount'] = {'column_index': 0}
+        column_index['input']['tax amount'] = {'column_index': 1}
+
+        index = 2
+
+        for i in range(len(unique_expense_desc)):
+            column_index['input'][unique_expense_desc[i]] = {'column_index': i + index}
+
+        index += len(unique_expense_desc)
+
+        for i in range(len(unique_tax_name)):
+            column_index['input'][unique_tax_name[i]] = {'column_index': i + index}
+
+        for i in range(len(unique_categories)):
+            column_index['output'][unique_categories[i]] = {'value': i}
+
+        Parser.__save_column_index(column_index)
+
+    @staticmethod
+    def __save_column_index(json_file):
+        with open('../data/column_index.txt', 'w') as outfile:
+            json.dump(json_file, outfile)
+
+Parser.parse_csv_data(Parser.training_data_file)
